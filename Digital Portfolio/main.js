4a1456b85d7263801608fe0f06a7994f6e2ccf19
platformer <platformer500@gmail.com>
2023-06-11 10:01:18 -0400

    Created moble navigation and added to work samples.

--- Digital Portfolio/main.js
+++ Digital Portfolio/main.js
@@ -1,8 +1,26 @@
 // navigation
+
+var navOpen = false;
+
 function openNav() {
     document.getElementById("nav").style.width = "200px";
+    navOpen = true;
 }
 
 function closeNav() {
     document.getElementById("nav").style.width = "0";
+    navOpen = false
 }
+
+window.addEventListener('resize', function () {
+    var screenWidth = window.innerWidth;
+
+    if (screenWidth <= 1000) {
+        if (navOpen == false) {
+            closeNav();
+        }
+    } else {
+        document.getElementById("nav").style.width = "500px";
+        navOpen = false;
+    }
+});
